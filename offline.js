394269975bae9f004b10821dee76647ac93c21ff
dudeimconfused <74417622+dudeimconfused@users.noreply.github.com>
2021-01-06 20:11:09 +0530

    papaya

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1609943790,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/button-sheet0.png",
+		"images/particles.png",
+		"images/blue-sheet0.png",
+		"images/notifybox-sheet0.png",
+		"images/notifyboxplatform-sheet0.png",
+		"images/tap-sheet0.png",
+		"media/pipe splash.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
